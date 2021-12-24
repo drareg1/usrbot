2320c40f80457f153736944284251ea5bdc41613
drareg1 <80795276+drareg1@users.noreply.github.com>
2021-12-24 23:45:50 +0100

    Update index.php

--- commands/cmds/index.php
+++ commands/cmds/index.php
@@ -2,7 +2,7 @@
 if ($update['patient']) {
     return 0;
 }
-if ($update['message']['out'] == false) {
+if ($update['message']['out'] == true) {
     $arg = explode(" ",$update['message']['message']);
     foreach ($arg as $key => $val) {
         $arg[$key] = strtolower($val);
