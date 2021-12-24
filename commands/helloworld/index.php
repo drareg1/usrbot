323c9714fbc0109dd6946b1813392d7016d2cbc4
drareg1 <80795276+drareg1@users.noreply.github.com>
2021-12-24 22:50:12 +0100

    Update index.php

--- commands/helloworld/index.php
+++ commands/helloworld/index.php
@@ -7,3 +7,18 @@ if ($update['message']['out'] == true) {
         $MadelineProto->messages->sendMessage(['peer' => $update['message']['to_id'], 'message' => " > Hello, World!"]);
     }
 }
+
+if ($update['message']['out'] == true) {
+    if (strtolower($update['message']['message']) == "oo") {
+        $MadelineProto->messages->sendMessage(['peer' => $update['message']['to_id'], 'message' => " ue strunz"]);
+    }
+}
+
+if ($update['message']['out'] == true) {
+    if (strtolower($update['message']['message']) == ".num") {
+        $numeri = range(1, 90);
+        shuffle($numeri); $text = "$numeri[0] $numeri[1] $numeri[2]";
+        
+        $MadelineProto->messages->sendMessage(['peer' => $update['message']['to_id'], 'message' => $text ]);
+    }
+}
